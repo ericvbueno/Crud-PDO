84fe9250965c797056c75c840e35a3189c930aa7
Eric Bueno <eric@DESKTOP-M3RC3GQ.localdomain>
2022-02-14 23:16:56 -0300

    [refactor] - Criando classe de conexão com o banco

---
+++ class-pessoa.php
@@ -0,0 +1,14 @@
+<?php
+    class Pessoa {
+    private $pdo;
+    //conexão com o banco;
+    public function __construct($dbname, $host, $user, $senha) {
+        try {
+            $this->pdo = new PDO("mysql:dbname=$dbname; host=$host", "$user", "$senha");
+        } catch (PDOException $e) {
+            echo "Erro: {$e->getMessage()}";
+            exit();
+        }
+    }
+};
+?>
--- conection.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    try {
-        $pdo = new PDO("mysql:dbname=crud; host=localhost", "root", "");
-    } catch (PDOException $e) {
-        echo "Falha: {$e->getMessage()}";
-    }
-?>
